5b7c9f02ae6a667caf1a7eb3b0fec4a41c06f160
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2022-09-23 21:34:59 +0300

    Lesson3.Homework

---
+++ _08.29.First_project/src/NewHomework.java
@@ -0,0 +1,25 @@
+public class NewHomework {
+
+        public static void main(String[] args) {
+
+            //Задача №1
+            int a = 10;
+            int b = 3;
+
+            System.out.println(String.format("a = %d", a));
+            System.out.println(String.format("b = %d", b));
+            System.out.println(String.format("a+b= %d", a + b));
+            System.out.println(String.format("a-b= %d", a - b));
+            System.out.println(String.format("a*b= %d", a * b));
+            System.out.println(String.format("a/b= %d", a / b));
+
+            int c = a % b;
+            System.out.println(String.format("остаток от деления a на b = %d", c));
+
+            //Экстра задача
+            System.out.println("\uD83D\uDC7D");
+            System.out.println("\u063A");
+
+        }
+
+}
